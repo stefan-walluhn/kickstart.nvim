c8b16f8b1f8be87fd81be32132622830ae029b6a
Stefan <stefan@neuland.io>
2025-05-06 17:07:58 +0200

    add rubocop linter

--- init.lua
+++ init.lua
@@ -739,6 +739,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'black',
         'prettier',
+        'rubocop',
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
