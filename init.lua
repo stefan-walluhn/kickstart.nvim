88928f0de4054ac1c65378c2ff8c28a7b5d64d4e
Stefan <stefan@neuland.io>
2026-05-11 13:15:41 +0200

    add missing file types

--- init.lua
+++ init.lua
@@ -278,6 +278,19 @@ vim.filetype.add {
     mjml = 'html',
     hbs = 'html',
   },
+  filename = { ['docker-compose.yml'] = 'yaml.docker-compose' },
+  pattern = {
+    ['.*/defaults/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/host_vars/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/group_vars/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/group_vars/.*/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/playbook.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/playbooks/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/roles/.*/tasks/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/roles/.*/handlers/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/tasks/.*%.ya?ml'] = 'yaml.ansible',
+    ['.*/molecule/.*%.ya?ml'] = 'yaml.ansible',
+  },
 }
 
 -- [[ Basic Keymaps ]]
